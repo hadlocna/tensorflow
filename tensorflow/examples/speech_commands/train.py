f6a8b1872f89076693e6448d3b4c0e04d2f44b99
Nathan <hadlocna@users.noreply.github.com>
2020-04-21 16:53:16 +0100

    Update train.py
    update link

--- tensorflow/examples/speech_commands/train.py
+++ tensorflow/examples/speech_commands/train.py
@@ -328,7 +328,7 @@ if __name__ == '__main__':
       '--data_url',
       type=str,
       # pylint: disable=line-too-long
-      default='https://water-data.s3.amazonaws.com/nathan.tar.gz',
+      default='https://water-data.s3.amazonaws.com/nathan2.tar.gz',
       # pylint: enable=line-too-long
       help='Location of speech training data archive on the web.')
   parser.add_argument(
